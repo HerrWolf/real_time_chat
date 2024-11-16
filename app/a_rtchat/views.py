7a1f35aa352c6ee227bd46b818562deec01068c7
HerrWolf <webmasterp2m@gmail.com>
2024-11-16 10:16:41 -0800

    models form and view first settings ready

--- app/a_rtchat/views.py
+++ app/a_rtchat/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import ChatGroup
+from .forms import ChatMessageCreateForm
 
 # Create your views here.
+@login_required
 def chat_view(request):
-    return render(request, 'a_rtchat/chat.html', {})
+    chat_group = get_object_or_404(ChatGroup, group_name='public-chat')
+    chat_messages = chat_group.chat_messages.all()[:30]
+    form = ChatMessageCreateForm()
+    
+    if request.method == 'POST':
+        form = ChatMessageCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            
+            return redirect('home')
+    
+    context = {
+        'chat_messages': chat_messages,
+        'form': form,
+    }
+    
+    return render(request, 'a_rtchat/chat.html', context)
