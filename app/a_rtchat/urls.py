6a811395abbaafe2ab6fb39a77d0b25faf41cdb9
HerrWolf <webmasterp2m@gmail.com>
2024-12-08 11:49:26 -0800

    private chat messages

--- app/a_rtchat/urls.py
+++ app/a_rtchat/urls.py
@@ -3,4 +3,6 @@ from a_rtchat.views import *
 
 urlpatterns = [    
     path('', chat_view, name='home'),
+    path('chat/<username>', get_or_create_chatroom, name='start-chat'),
+    path('chat/room/<chatroom_name>', chat_view, name='chatroom'),
 ]
