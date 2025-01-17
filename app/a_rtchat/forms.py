bb5ad1569eae76064ab35d7e62ce5e00e594ba41
HerrWolf <webmasterp2m@gmail.com>
2025-01-17 09:20:51 -0800

    Live activity monitoring finish

--- app/a_rtchat/forms.py
+++ app/a_rtchat/forms.py
@@ -8,5 +8,30 @@ class ChatMessageCreateForm(ModelForm):
         model = GroupMessage
         fields = ['body']
         widgets = {
-            'body': forms.TextInput(attrs={'placeholder':'Add message ...', 'class': 'p-4 text-black', 'maxlength': '300', 'autofocus': True})
+            'body': forms.TextInput(attrs={'placeholder':'Add message ...', 'class': 'p-4 text-black', 'maxlength': '300', 'autofocus': True, 'autocomplete': 'off'})
+        }
+        
+
+class NewGroupForm(ModelForm):
+    class Meta:
+        model = ChatGroup
+        fields = ['groupchat_name']
+        widgets = {
+            'groupchat_name': forms.TextInput(attrs={
+                'placeholder':'Add name ...', 
+                'class': 'p-4 text-black', 
+                'maxlength': '128',
+                'autofocus': True
+                })
+        }
+        
+class ChatGroupEditForm(ModelForm):
+    class Meta:
+        model = ChatGroup
+        fields = ['groupchat_name']
+        widgets = {
+            'groupchat_name': forms.TextInput(attrs={
+                'class': 'p-4 text-xl font-bold mb-4', 
+                'maxlength': '300'
+                }),
         }
